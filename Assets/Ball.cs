fc2f03044de7567639cb4a7e7b472d1bf75d72f5
Rodrigo Dias <rodrigo@rodderscode.co.uk>
2016-05-31 00:21:35 +0100

    moving all to latest version

--- Assets/Ball.cs
+++ Assets/Ball.cs
@@ -14,13 +14,19 @@ public class Ball : MonoBehaviour {
 	}
 	
 	void OnCollisionEnter2D(Collision2D coll){
-        // GameObject player = coll.gameObject;
+        // GameObject obj = coll.gameObject;
+        //get the distances between the collider and the ball to apply as padding 
+		// so the ball is not ON the obj, but where it ws hit first
+
+        // float distX = this.position.x - coll.transform.position.x;
+        // float distY = this.position.y - coll.transform.position.y;
+
+        // this.transform.position = new Vector3(obj.position.x + distX, obj.position.y + distY, obj.position.z);
+        // print("Applied!");
         // ContactPoint2D[] contactPoints = coll.contacts;
-        foreach(ContactPoint2D hit in coll.contacts)
-		{
-        	Debug.Log(hit.point);
-        }
-        // this.transform.position = player.position;
+  		// foreach(ContactPoint2D hit in coll.contacts){
+		// 	Debug.Log(hit.point);
+		// }
      	// transform.rotation = Quaternion.Euler(0, Cam.rotation.eulerAngles.y, 0);
         //Destroy(gameObject);		 
 	}
