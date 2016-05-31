69af5c58efe4b52d84273b93df875e4ade5a0de7
Rodrigo Dias <rodrigo@rodderscode.co.uk>
2016-05-31 01:27:01 +0100

    I dont know where all this shit comes from... i swear

--- Assets/Camera.cs
+++ Assets/Camera.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 
 public class Camera : MonoBehaviour {
+    public Ball ball; 
 
 	// Use this for initialization
 	void Start () {
@@ -10,6 +11,6 @@ public class Camera : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+	   Debug.Log(ball);
 	}
 }
